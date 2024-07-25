86caf7dd22f2149a9fc00ea860d02d3ad61d9815
vigneshM138 <vigneshmb2001@gmail.com>
2024-07-25 21:27:28 +0530

    Projects Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import Projects from "./Components/Projects/Projects";
 import Skills from "./Components/Skills/Skills";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
---
+++ frontend/src/Components/Projects/Projects.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Projects.css";
+import pro from "./pro.png";
+import { NavLink } from "react-router-dom";
+import { FiGithub } from "react-icons/fi";
+import { TbLivePhoto } from "react-icons/tb";
+
+const Projects = () => {
+  return (
+    <div className="projects_page">
+      <h1>Projects</h1>
+      <div className="projects">
+        <div className="project">
+          <div className="project_img">
+            <img src={pro} alt="" />
+          </div>
+          <div className="project_name">
+            <h5>project name</h5>
+          </div>
+          <div className="project_details">
+            <p>
+              My static front-end website showcases elegant design and seamless
+              user experience, highlighting my proficiency in creating visually
+              appealing and responsive web interfaces
+            </p>
+          </div>
+          <div className="project_links">
+            <a href=""><FiGithub />code</a>
+            <a href=""><TbLivePhoto />live</a>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
