7d8887cb8ee1643b132f001824fdddab753c8c7f
vigneshM138 <vigneshmb2001@gmail.com>
2024-07-26 21:42:53 +0530

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Projects from "./Components/Projects/Projects";
@@ -15,6 +16,7 @@ function App() {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        © All Rights Reserved By <span>Vignesh</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
